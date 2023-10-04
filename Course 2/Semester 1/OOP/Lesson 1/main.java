82c484ceb84cbc879426d1085cf220d5a0445476
Kraziman <notkraziman@gmail.com>
2023-10-04 10:28:06 +0300

    New Paths for C2S1, Assignment 1 added

---
+++ Course 2/Semester 1/OOP/Lesson 1/main.java
@@ -0,0 +1,10 @@
+public static void main(String[]args){
+    for (int i = 0; i <= 300; i++){
+        if (i % 2 = 0){
+            continue;
+        }
+        else{
+            system.out.println(i);
+        }
+    }
+}
