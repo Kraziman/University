097e47bddd4627de631ea4cf0b5e650af0e777d5
Kraziman <70752688+Kraziman@users.noreply.github.com>
2023-10-04 14:37:29 +0300

    Assignment 6
    Assignment 6 - Print every number from 1 to 300 that is divisible by the prime numbers in the same range

---
+++ Course 2/Semester 1/OOP/Lesson 1/Assingment 6/src/Assingment6.java
@@ -0,0 +1,31 @@
+public class Assingment6 {
+    public static void main(String[] args) {
+        int numLim = 300;
+        for (int i = 2; i < numLim; i ++){
+            int j = 2;
+            boolean isPrime = true;
+            boolean canDivide = false;
+
+            do {
+                if (i % j == 0 && i != j){
+                    isPrime = false;
+                    break;
+                }
+                j++;
+            } while(j < i);
+
+            for (int k = 1; k < 300; k++){
+                if (isPrime){
+                    if (k % i == 0){
+                        canDivide = true;
+                    }
+                }
+
+                if (canDivide){
+                    System.out.println(k + " is divisable by a prime number in the range of 1 to 300");
+                    break;
+                }
+            }
+        }
+    }
+}
